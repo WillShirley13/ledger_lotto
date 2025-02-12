039b9b97a8539252cd0493dcbf9b979f9c594dca
Will Shirley <willshirley13@gmail.com>
2025-02-12 20:32:08 +0000

    update font in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer"
 import { Titan_One } from "next/font/google";
 import { Providers } from "./components/anchor/Providers";
-import IsPayoutTime from "./components/IsPayoutTime";
 
 const titan = Titan_One({
 	weight: ["400"],
+	subsets: ['latin'],
 });
 
 export default function RootLayout({
